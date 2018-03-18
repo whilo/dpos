f3593120a3bcedd6b67a74f3ae20de54bab9ab6f
Christian Weilbach <christian@topiq.es>
2018-03-18 23:49:06 +0100

    Add weighted stake voting.

--- src/dpos/delegation.clj
+++ src/dpos/delegation.clj
@@ -12,12 +12,15 @@
 ;;   + need some statistics about nodes to allow community control,
 ;;     e.g. amounts of blocks dropped, can be part of chain
 ;;   + update connections after each received block
+;;   + transitive voting?
+;;   + self voting?
+
 
 
 (def registry {:peerA {:balance 5000
                        :approvals #{:peerB}
                        :address "wss://meta.serv1.us:38237"}
-               :peerB {:balance 200
+               :peerB {:balance 2000
                        :approvals #{:peerC}
                        :address "wss://meta.meinserver.de:3333"}
                :peerC {:balance 3000
@@ -32,9 +35,20 @@
 
 (approval-voting registry)
 
+(defn weighted-approval-voting [registry]
+  (->> (vals registry)
+     (reduce (fn [acc e]
+               (reduce #(update %1 %2 (fnil + 0) (:balance e))
+                       acc
+                       (seq (:approvals e))))
+             {})))
+
+
+(weighted-approval-voting registry)
+
 
 (defn top [registry n]
-  (->> (approval-voting registry)
+  (->> (weighted-approval-voting registry)
      (sort-by val)
      reverse
      (take n)
