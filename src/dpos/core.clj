85efd4de6a54f2a05c40802cb1cadff73701e708
Christian Weilbach <christian@topiq.es>
2018-03-18 21:01:15 +0100

    fiat lux.

---
+++ src/dpos/core.clj
@@ -0,0 +1,128 @@
+(ns dpos.core
+  (:require [com.rpl.specter :refer :all]
+            [hasch.core :refer [uuid]]))
+
+
+;; simple DPoS
+
+
+(def quorum (/ 2 3))
+
+
+(def genesis {:parent nil
+              :pos 0
+              :txs []})
+
+
+
+(def init
+  {:peerA {:head (uuid genesis)
+           :id :peerA
+           :pending []
+           :blocks {(uuid genesis) genesis}}
+   :peerB {:head (uuid genesis)
+           :id :peerB
+           :pending []
+           :blocks {(uuid genesis) genesis}}
+   :peerC {:head (uuid genesis)
+           :id :peerC
+           :pending []
+           :blocks {(uuid genesis) genesis}}})
+
+
+
+;; model sequence of steps
+;; in each step peer is selected
+
+(defn send-transaction [state peer-id tx]
+  (update-in state [peer-id :pending] conj tx))
+
+
+
+(defn create-block [peer]
+  (let [{:keys [chain pending id blocks]} peer
+        max-block (apply max-key :pos (vals blocks))
+        new-block {:txs pending
+                   :pos (inc (:pos max-block))
+                   :parent (uuid max-block)}]
+    {:head new-block
+     :pending []
+     :id id
+     :blocks (assoc blocks (uuid new-block) new-block)}))
+
+
+
+(defn send-new-block [state peer]
+  (let [block (:head (state peer))]
+    (transform [MAP-VALS #(not= peer (:id %)) :blocks]
+               #(assoc % (uuid block) block)
+               state)))
+
+
+(comment
+  (send-new-block init :peerA))
+
+
+
+(defn find-longest-chain [peer]
+  (let [{:keys [blocks]} peer
+        m (apply max-key :pos (vals blocks))]
+    (loop [chain []
+           b m]
+      (if b
+        (recur (conj chain b) (blocks (:parent b)))
+        (vec (reverse chain))))))
+
+
+(defn settled-chain [chain peers]
+  (let [c (count peers)]
+    (drop-last (Math/ceil (* quorum c)) chain)))
+
+
+;; stateful
+
+
+(def state (atom init))
+
+
+(clojure.pprint/pprint @state)
+
+(defn peer-order [peers steps]
+  (->> peers 
+       cycle
+       (take steps)
+       #_shuffle
+       vec))
+
+
+;; perfect simulation
+(defn simulate [steps]
+  (let [block-order (peer-order (keys init) steps)]
+    (for [i (range steps)]
+      (swap! state
+             (fn [old]
+               (let [peer (block-order i)]
+                 (-> old
+                    (send-transaction peer i)
+                    (update peer create-block)
+                    (send-new-block peer))))))))
+
+
+(simulate 10)
+
+(def longest (find-longest-chain (:peerA @state)))
+
+(def settled (settled-chain longest (keys init)))
+
+
+(reduce + (mapcat :txs longest)) ;; => 45
+
+
+;; open questions
+;; - how is delegate membership modeled
+;; - how is time and peer<->block sequence agreed upon?
+;; - model double spending attack
+
+;; orthogonal design questions
+;; - collect fees due to tx type (and cost)
+;; - ensure public key integrity
